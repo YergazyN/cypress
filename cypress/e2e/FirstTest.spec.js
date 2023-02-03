eb99f7a5740db82ac4f723d95f34c5a1a0150728
Yergazy <nurmaganbetov0486@gmail.com>
2023-02-03 09:53:31 +0600

    confirm window check

--- cypress/e2e/FirstTest.spec.js
+++ cypress/e2e/FirstTest.spec.js
@@ -206,7 +206,7 @@ describe("Our first suite", () => {
     });
   });
 
-  it.only("Web tables", () => { 
+  it("Web tables", () => { 
     cy.visit("/");
     cy.contains("Tables & Data").click();
     cy.contains("Smart Table").click();
@@ -267,4 +267,48 @@ describe("Our first suite", () => {
         });
       });
   });
+
+
+  // popups and tooltips
+
+  it( 'tooltip', () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Tooltip").click();
+
+    cy.contains('nb-card', 'Colored Tooltips')
+        .contains('Default').click();
+
+    cy.get('nb-tooltip').should('contain', 'This is a tooltip');
+
+  });
+
+  // confirm dialog
+  it.only('confirm dialog', ()=> {
+    cy.visit("/");
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+
+    //cy.get('tbody tr').first().find('.nb-trash').click();
+    // 1
+    // cy.on('window confirm', confirm => {
+    //     expect(confirm).to.equal('Are you sure you want to delete?');
+    // })
+
+    // 2
+    const stub = cy.stub()
+    cy.on('window:confirm', stub)
+    cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')  
+    });
+
+    // 3
+
+    // cy.get('tbody tr').first().find('.nb-trash').click();
+    
+    // cy.on('window confirm', confirm => false)
+
+    
+  });
+
 });
